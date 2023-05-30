36be36f10b73b2cd556dd95c476afa999c456d1e
Parth0325 <134394228+Parth0325@users.noreply.github.com>
2023-05-30 09:57:36 -0400

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -3,47 +3,25 @@ package exercise1;
 import java.util.Random;
 import java.util.Scanner;
 
-/**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
- * 
- * Modified by [Your Name] May 2023
- */
 public class Card {
 
-    private String suit; //clubs, spades, diamonds, hearts
-    private int value;//1-13
+   private String suit;
+   private int value;
 
-    public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+   public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
 
-    /**
-     * @return the suit
-     */
     public String getSuit() {
         return suit;
     }
 
-    /**
-     * @param suit the suit to set
-     */
     public void setSuit(String suit) {
         this.suit = suit;
     }
 
-    /**
-     * @return the value
-     */
     public int getValue() {
         return value;
     }
 
-    /**
-     * @param value the value to set
-     */
     public void setValue(int value) {
         this.value = value;
     }
@@ -84,25 +62,7 @@ public class Card {
         }
     }
 
-    /**
-     * A simple method to print out personal information. Follow the instructions to
-     * replace this information with your own.
-     */
     private static void printInfo() {
 
-        System.out.println("Congratulations, you guessed right!");
-        System.out.println();
+       
 
-        System.out.println("My name is [Your Name]");
-        System.out.println();
-
-        System.out.println("My career ambitions:");
-        System.out.println("-- Add your career ambitions here");
-        System.out.println();
-
-        System.out.println("My hobbies:");
-        System.out.println("-- Add your hobbies here");
-
-        System.out.println();
-    }
-}
